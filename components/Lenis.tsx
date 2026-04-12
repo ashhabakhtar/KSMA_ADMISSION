3906dcf1b57b183c9b5b6d4f6a30a0868b401a83
ashhab akhtar <akhtarashhab@gmail.com>
2026-04-13 02:24:30 +0530

    fix(lenis): correct syntax error in Lenis initialization

--- components/Lenis.tsx
+++ components/Lenis.tsx
@@ -11,10 +11,10 @@ export default function SmoothScrollWrapper({ children }: SmoothScrollProps) {
   const lenisRef = useRef<any>(null)
 
   useEffect(() => {
-const lenis = new Lenis({
+    const lenis = new Lenis({
       duration: 1.2,
-      easing: (t: number) => Math.min(1,
-
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    })
     function raf(time: number) {
       lenis.raf(time)
       requestAnimationFrame(raf)
